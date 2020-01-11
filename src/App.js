e237b1e1bee4fa58ad6ecae45377ecce181d90d8
Joshua Tu <tjh8523@gmail.com>
2020-01-12 00:22:29 +1100

    homepage layout created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Homepage from "./homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Homepage />
     </div>
   );
 }
