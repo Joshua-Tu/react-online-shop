2760b951ae4f2eeae0a3026acd8c4f922bf8d3db
Joshua Tu <tjh8523@gmail.com>
2020-01-16 03:42:11 +1100

    shop data imported, started to do the shop page with collections

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
 import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+
 import "./App.css";
-import Homepage from "./pages/homepage/homepage.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const TopicList = props => {
-  console.log(props);
-
-  return (
-    <div>
-      <h1>Topic List</h1>
-      <Link to={`${props.match.url}/1`}>TO TOPIC 1</Link>
-      <Link to={`${props.match.url}/2`}>TO TOPIC 2</Link>
-      <Link to={`${props.match.url}/3`}>TO TOPIC 3</Link>
-    </div>
-  );
-};
-
-const TopicDetail = props => {
-  console.log(props);
-
-  return (
-    <div>
-      <Link to="/">Go Back Home</Link>
-      <button onClick={() => props.history.push("/topics")}>Topic List</button>
-      <h1>Topic Detail</h1>
-    </div>
-  );
-};
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/homepage/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" exact component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
-        <Route path="/topics" exact component={TopicList} />
-        <Route path="/topics/:topicId" component={TopicDetail} />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
